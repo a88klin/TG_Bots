b3a6432a033495fc7975e09f2a4a60eb19d0cffe
a88klin <a88.klin@gmail.com>
2024-04-27 15:14:23 +0300

    Update

--- TG_Bot_HR/processing/vacansy_processing.py
+++ TG_Bot_HR/processing/vacansy_processing.py
@@ -8,5 +8,6 @@ DB_INDEX_FILES = settings.db_index_files
 os.environ["OPENAI_API_KEY"] = settings.openai_api_key.get_secret_value()
 
 db_index_vacancies = FAISS.load_local(folder_path=DB_INDEX_FILES,
+                                      allow_dangerous_deserialization=True,
                                       embeddings=OpenAIEmbeddings(),
                                       index_name='db_vacancies_index')
